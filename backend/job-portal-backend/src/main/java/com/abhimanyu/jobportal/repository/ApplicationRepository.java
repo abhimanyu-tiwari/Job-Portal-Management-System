4cfe8a78163f42e050db1b29ef5b076d2d48ada8
ABH886 <tiwariji8632@gmail.com>
2026-09-03 17:02:20 +0530

    Add application access control

--- backend/job-portal-backend/src/main/java/com/abhimanyu/jobportal/repository/ApplicationRepository.java
+++ backend/job-portal-backend/src/main/java/com/abhimanyu/jobportal/repository/ApplicationRepository.java
@@ -1,7 +1,6 @@
 package com.abhimanyu.jobportal.repository;
 
 import com.abhimanyu.jobportal.entity.Application;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
